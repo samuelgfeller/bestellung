fcce910689e853829e12e5c55a5c7999d85ac0c7
Jannis <jannis.kirschner@gibmit.ch>
2019-01-03 15:16:46 +0100

    Update Feedback.php

--- model/entity/Feedback.php
+++ model/entity/Feedback.php
@@ -10,9 +10,11 @@ require_once __DIR__ . '/../Populate.php';
 class Feedback {
     public static function add($feedback,$client_id) {
         $db = Db::instantiate();
-        $query ='INSERT INTO feedback (feedback,kunde_id,zeit) VALUES ("'.$feedback.'",'.$client_id.',now());';
-        $result = $db->query($query);
-        Db::checkConnection($result,$query);
+        $query ='INSERT INTO feedback (feedback,kunde_id,zeit) VALUES (?,?,now());';
+        $stmt = $db->prepare($query);
+        $stmt->bind_param("si",$feedback,$client_id);
+        $stmt->execute();
+        Db::checkConnection($stmt,$query);
         return $db->insert_id;
     }
 }
